bf7863ff7ef2f634778c866b4c4f77614f7be2e0
Stjoo0925 <stjoo0925@gmail.com>
2024-05-13 22:50:36 +0900

    fixes : add jdbc

--- src/main/java/com/ohgiraffers/library/dto/MemberDTO.java
+++ src/main/java/com/ohgiraffers/library/dto/MemberDTO.java
@@ -1,21 +1,28 @@
-package com.ohgiraffers.dto;
+package com.ohgiraffers.library.dto;
 
 public class MemberDTO {
 
     private int memberNum;
     private String memberName;
     private String memberRentalList;
+    private String memberStatus;
 
    // MemberDTO -------------------------------------------------------------------------------------------------------
 
-    public MemberDTO(int memberNum, String memberName, String memberRentalList) {
+    public MemberDTO(int memberNum, String memberName, String memberRentalList, String memberStatus) {
         this.memberNum = memberNum;
         this.memberName = memberName;
         this.memberRentalList = memberRentalList;
+        this.memberStatus = memberStatus;
     }
 
     public MemberDTO() {
 
+    }
+
+    public MemberDTO(String memberName, int memberNum) {
+        this.memberName = memberName;
+        this.memberNum = memberNum;
     }
 
     // Getter & Setter ------------------------------------------------------------------------------------------------
@@ -44,14 +51,22 @@ public class MemberDTO {
         this.memberRentalList = memberRentalList;
     }
 
+    public String getMemberStatus() {
+        return memberStatus;
+    }
+
+    public void setMemberStatus(String memberStatus) {
+        this.memberStatus = memberStatus;
+    }
+
     // toString -------------------------------------------------------------------------------------------------------
 
     @Override
     public String toString() {
-        return "MemberDTO{" +
-                "memberNum=" + memberNum +
-                ", memberName='" + memberName + '\'' +
-                ", memberRentalList='" + memberRentalList + '\'' +
-                '}';
+        return  '[' + "회원번호 : " + memberNum +
+                ", 회원명 : '" + memberName + '\'' +
+                ", 회원이 대출중인 도서 : '" + memberRentalList + '\'' +
+                ", 회원등록상태 : '" + memberStatus + '\'' +
+                "]\n";
     }
 }
