4c1125e6208dca2c8e1f5c12651b544a4e36a4df
hwang <hjeonghan0@gmail.com>
2024-05-13 17:59:10 +0900

    2024.05.13

---
+++ src/main/java/com/ohgiraffers/Application.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers;
+
+import com.ohgiraffers.view.MemberView;
+
+public class Application {
+    public static void main(String[] args) {
+        MemberView.run();
+
+    }
+}
